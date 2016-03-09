a6764f4c862884b96497e9a31332b67070b0d371
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-03-09 20:49:07 +0200

    Add v1.0 source code

---
+++ Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OptionsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SetFontToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SetColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SetBackgroundColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MoreAppsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.FontDialog1 = New System.Windows.Forms.FontDialog()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.OptionsToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(578, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.OpenToolStripMenuItem, Me.SaveToolStripMenuItem, Me.ToolStripMenuItem1, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(44, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(135, 26)
+        Me.NewToolStripMenuItem.Text = "New"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(135, 26)
+        Me.OpenToolStripMenuItem.Text = "Open"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(135, 26)
+        Me.SaveToolStripMenuItem.Text = "Save As"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(132, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(135, 26)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'OptionsToolStripMenuItem
+        '
+        Me.OptionsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SetFontToolStripMenuItem, Me.SetColorToolStripMenuItem, Me.SetBackgroundColorToolStripMenuItem})
+        Me.OptionsToolStripMenuItem.Name = "OptionsToolStripMenuItem"
+        Me.OptionsToolStripMenuItem.Size = New System.Drawing.Size(73, 24)
+        Me.OptionsToolStripMenuItem.Text = "Options"
+        '
+        'SetFontToolStripMenuItem
+        '
+        Me.SetFontToolStripMenuItem.Name = "SetFontToolStripMenuItem"
+        Me.SetFontToolStripMenuItem.Size = New System.Drawing.Size(228, 26)
+        Me.SetFontToolStripMenuItem.Text = "Set Font"
+        '
+        'SetColorToolStripMenuItem
+        '
+        Me.SetColorToolStripMenuItem.Name = "SetColorToolStripMenuItem"
+        Me.SetColorToolStripMenuItem.Size = New System.Drawing.Size(228, 26)
+        Me.SetColorToolStripMenuItem.Text = "Set Font Color"
+        '
+        'SetBackgroundColorToolStripMenuItem
+        '
+        Me.SetBackgroundColorToolStripMenuItem.Name = "SetBackgroundColorToolStripMenuItem"
+        Me.SetBackgroundColorToolStripMenuItem.Size = New System.Drawing.Size(228, 26)
+        Me.SetBackgroundColorToolStripMenuItem.Text = "Set Background Color"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MoreAppsToolStripMenuItem, Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(53, 24)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'MoreAppsToolStripMenuItem
+        '
+        Me.MoreAppsToolStripMenuItem.Name = "MoreAppsToolStripMenuItem"
+        Me.MoreAppsToolStripMenuItem.Size = New System.Drawing.Size(155, 26)
+        Me.MoreAppsToolStripMenuItem.Text = "More apps"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(155, 26)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Font = New System.Drawing.Font("Consolas", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(0, 31)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(578, 231)
+        Me.TextBox1.TabIndex = 1
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.DefaultExt = "txt"
+        Me.SaveFileDialog1.FileName = "*.txt"
+        Me.SaveFileDialog1.Filter = "Text files|*.txt"
+        Me.SaveFileDialog1.ValidateNames = False
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Filter = "Text files|*.txt"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(578, 262)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "SimplePad"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MoreAppsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents OptionsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem3 As ToolStripSeparator
+    Friend WithEvents SetColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SetFontToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SetBackgroundColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FontDialog1 As FontDialog
+    Friend WithEvents ColorDialog1 As ColorDialog
+End Class
---
+++ Form1.vb
@@ -0,0 +1,108 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.ScrollBars = ScrollBars.Vertical
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles MyBase.FormClosing
+        If TextBox1.Modified Then
+            Dim ask As MsgBoxResult
+            ask = MsgBox("Do you want to save the changes you made?", MsgBoxStyle.YesNoCancel, "Save Changes")
+            If ask = MsgBoxResult.No Then
+            ElseIf ask = MsgBoxResult.Cancel Then
+                e.Cancel = True
+            ElseIf ask = MsgBoxResult.Yes Then
+                If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+                    My.Computer.FileSystem.WriteAllText(SaveFileDialog1.FileName, TextBox1.Text, False)
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+            My.Computer.FileSystem.WriteAllText(SaveFileDialog1.FileName, TextBox1.Text, False)
+        End If
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Close()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub ToolStripMenuItem2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub MoreAppsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MoreAppsToolStripMenuItem.Click
+        Process.Start("http://dvt32.blogspot.com/")
+    End Sub
+
+    Private Sub SaveFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles SaveFileDialog1.FileOk
+
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        If TextBox1.Modified Then
+            Dim ask As MsgBoxResult
+            ask = MsgBox("Do you want to save the changes you made?", MsgBoxStyle.YesNoCancel, "New Document")
+            If ask = MsgBoxResult.No Then
+                TextBox1.Clear()
+            ElseIf ask = MsgBoxResult.Cancel Then
+            ElseIf ask = MsgBoxResult.Yes Then
+                If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+                    My.Computer.FileSystem.WriteAllText(SaveFileDialog1.FileName, TextBox1.Text, False)
+                    TextBox1.Clear()
+                End If
+            Else
+                TextBox1.Clear()
+            End If
+        End If
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        Dim message = "SimplePad v1.0" + Environment.NewLine + Environment.NewLine + "More apps @ http://dvt32.blogspot.com "
+        MsgBox(message, MsgBoxStyle.Information, "About SimplePad")
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        If TextBox1.Modified Then
+            Dim ask As MsgBoxResult
+            ask = MsgBox("Do you want to save the changes you made?", MsgBoxStyle.YesNoCancel, "Open Document")
+            If ask = MsgBoxResult.No Then
+                OpenFileDialog1.ShowDialog()
+                TextBox1.Text = My.Computer.FileSystem.ReadAllText(OpenFileDialog1.FileName)
+            ElseIf ask = MsgBoxResult.Cancel Then
+            ElseIf ask = MsgBoxResult.Yes Then
+                If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+                    My.Computer.FileSystem.WriteAllText(SaveFileDialog1.FileName, TextBox1.Text, False)
+                End If
+                OpenFileDialog1.ShowDialog()
+                TextBox1.Text = My.Computer.FileSystem.ReadAllText(OpenFileDialog1.FileName)
+            End If
+        Else
+            OpenFileDialog1.ShowDialog()
+            TextBox1.Text = My.Computer.FileSystem.ReadAllText(OpenFileDialog1.FileName)
+        End If
+    End Sub
+
+    Private Sub SetFontToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SetFontToolStripMenuItem.Click
+        If FontDialog1.ShowDialog() = DialogResult.OK Then
+            TextBox1.Font = FontDialog1.Font
+        End If
+    End Sub
+
+    Private Sub SetColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SetColorToolStripMenuItem.Click
+        If ColorDialog1.ShowDialog() = DialogResult.OK Then
+            TextBox1.ForeColor = ColorDialog1.Color
+        End If
+    End Sub
+
+    Private Sub SetBackgroundColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SetBackgroundColorToolStripMenuItem.Click
+        If ColorDialog1.ShowDialog() = DialogResult.OK Then
+            TextBox1.BackColor = ColorDialog1.Color
+        End If
+    End Sub
+End Class
